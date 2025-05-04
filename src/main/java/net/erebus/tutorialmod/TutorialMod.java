62190dd8501a6412472bddbd426aacba4504c90f
Erebus5421 <poodlemaster66@outlook.com>
2025-05-04 21:15:12 +1000

    Two new items added Raw Garnet and Garnet!

--- src/main/java/net/erebus/tutorialmod/TutorialMod.java
+++ src/main/java/net/erebus/tutorialmod/TutorialMod.java
@@ -1,5 +1,6 @@
 package net.erebus.tutorialmod;
 
+import net.erebus.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -12,6 +13,6 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
